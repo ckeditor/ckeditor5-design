04a9ca4f297d91128d4261b062cc4bd722646eea
Grzegorz Pabian <greg.pabian@gmail.com>
2014-11-04 16:16:46 +0100

    Works on dev repo structure and referencing.

--- gruntfile.js
+++ gruntfile.js
@@ -1,101 +1,29 @@
 module.exports = function( grunt ) {
+	require( 'load-grunt-tasks' )( grunt );
+
 	grunt.initConfig( {
-		pkg: grunt.file.readJSON( 'package.json' ),
-
-		jshint: {
-			files: [ '*.js' ],
-			options: jshintConfig
+		clean: {
+			build: [ 'build' ]
 		},
 
-		jscs: {
-			src: '*.js',
-			options: jscsConfig
-		},
-
-		githooks: {
-			all: {
-				'pre-commit': 'default'
+		requirejs: {
+			build: {
+				options: {
+					almond: true,
+					baseUrl: 'node_modules/ckeditor-core/src/',
+					include: [ 'ckeditor', 'plugins/example' ],
+					optimize: 'none',
+					out: 'build/ckeditor.js',
+					packages: [ {
+						name: 'plugins/example',
+						location: '../../ckeditor-plugin-example/src/',
+						main: 'example'
+					} ]
+				}
 			}
 		}
 	} );
 
-	// Load all grunt plugins.
-	require( 'load-grunt-tasks' )( grunt );
-
-	// Default tasks.
-	grunt.registerTask( 'default', [ 'jshint', 'jscs' ] );
-};
-
-// Configurations for JSHint
-var jshintConfig = {
-};
-
-// Configurations for JSCS (JavaScript Code Style checker)
-var jscsConfig = {
-	'excludeFiles': [
-		'node_modules/*'
-	],
-	'requireCurlyBraces': [
-		'if', 'else', 'for', 'while', 'do', 'switch', 'try', 'catch'
-	],
-	'requireSpaceAfterKeywords': [
-		'if', 'else', 'for', 'while', 'do', 'switch', 'return', 'try', 'catch'
-	],
-	'requireSpaceBeforeBlockStatements': true,
-	'requireParenthesesAroundIIFE': true,
-	'requireSpacesInConditionalExpression': {
-		'afterTest': true,
-		'beforeConsequent': true,
-		'afterConsequent': true,
-		'beforeAlternate': true
-	},
-	'requireSpacesInFunctionExpression': {
-		'beforeOpeningCurlyBrace': true
-	},
-	'disallowSpacesInFunctionExpression': {
-		'beforeOpeningRoundBrace': true
-	},
-	'requireBlocksOnNewline': true,
-	'requireSpacesInsideObjectBrackets': 'all',
-	'requireSpacesInsideArrayBrackets': 'all',
-	'disallowSpaceAfterObjectKeys': true,
-	'requireCommaBeforeLineBreak': true,
-	'requireOperatorBeforeLineBreak': [
-		'?', '=', '+', '-', '/', '*', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=',
-		'-=', '/=', '^='
-	],
-	'requireSpaceBeforeBinaryOperators': [
-		'+', '-', '/', '*', '=', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=', '-=',
-		'/=', '^='
-	],
-	'requireSpaceAfterBinaryOperators': [
-		'+', '-', '/', '*', '=', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=', '-=',
-		'/=', '^='
-	],
-	'disallowSpaceAfterPrefixUnaryOperators': [
-		'++', '--', '+', '-', '~', '!'
-	],
-	'disallowSpaceBeforePostfixUnaryOperators': [
-		'++', '--'
-	],
-	'disallowKeywords': [
-		'with'
-	],
-	'validateLineBreaks': 'LF',
-	'validateQuoteMarks': {
-		'mark': '\'',
-		'escape': true
-	},
-	'validateIndentation': '\t',
-	'disallowMixedSpacesAndTabs': true,
-	'disallowTrailingWhitespace': true,
-	'disallowKeywordsOnNewLine': [
-		'else', 'catch'
-	],
-	'maximumLineLength': 120,
-	'safeContextKeyword': [
-		'that'
-	],
-	'requireDotNotation': true,
-	'disallowYodaConditions': true
+	grunt.registerTask( 'build', [ 'clean', 'requirejs' ] );
+	grunt.registerTask( 'default', [ 'build' ] );
 };
