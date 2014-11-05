7e671d848e7f8e67a2c088159cda94b6c64f1033
Grzegorz Pabian <greg.pabian@gmail.com>
2014-11-05 11:19:49 +0100

    Added require plugin to load CKEDITOR plugins.

--- ckeditor.js
+++ ckeditor.js
@@ -1,22 +1,21 @@
 requirejs.config( {
-	baseUrl: '../node_modules/ckeditor-core/src/',
+	baseUrl: '../node_modules/ckeditor-core/'
 
-	packages: [ {
-		name: 'plugins/example',
-		location: '../../ckeditor-plugin-example/src/',
-		main: 'example'
-	} ]
+	// packages: [ {
+	// 	name: 'plugins/example',
+	// 	location: '../ckeditor-plugin-example/src/'
+	// } ]
 } );
 
-var CKEDITOR = CKEDITOR || {};
+// var CKEDITOR = CKEDITOR || {};
 
-CKEDITOR.require = CKEDITOR.require || require;
-CKEDITOR.define = CKEDITOR.define || define;
+// CKEDITOR.require = CKEDITOR.require || require;
+// CKEDITOR.define = CKEDITOR.define || define;
 
 require( [
 	'mvc',
-	'plugins/example',
-	'plugins/example/foo'
+	'plugins!example',
+	'plugins!example/foo'
 ], function(
 	MVC,
 	example,
