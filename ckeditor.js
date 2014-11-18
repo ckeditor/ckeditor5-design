7c5c577f4895c8296b62667a78a9ad8e2152bec7
Grzegorz Pabian <greg.pabian@gmail.com>
2014-11-18 09:47:35 +0100

    Modified code to work with the actual core code and API.

--- ckeditor.js
+++ ckeditor.js
@@ -2,6 +2,36 @@ requirejs.config( {
 	baseUrl: '../node_modules/ckeditor-core/src/'
 } );
 
-window.CKEDITOR = window.CKEDITOR || {};
-CKEDITOR.define = CKEDITOR.define || define;
-CKEDITOR.require = CKEDITOR.require || require;
+( function( root ) {
+	// create CKEDITOR namespace
+	root.CKEDITOR = root.CKEDITOR || {};
+	CKEDITOR.define = CKEDITOR.define || define;
+	CKEDITOR.require = CKEDITOR.require || require;
+
+	var createBuffer = [];
+
+	// buffer create calls
+	CKEDITOR.create = function( selector, options ) {
+		createBuffer.push( arguments );
+	};
+
+	// create editor instances from the buffer
+	function create() {
+		var options = createBuffer.shift();
+
+		if ( !options ) {
+			return;
+		}
+
+		CKEDITOR.create.apply( null, options );
+
+		create();
+	}
+
+	require( [ 'api', 'tools/utils' ], function( api, utils ) {
+		// extend CKEDITOR namespace with the public API
+		// overrides a temporary implementation of the "create" method
+		utils.extend( root.CKEDITOR, api );
+		create();
+	} );
+} )( this );
