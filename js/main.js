0f344d27ee603c2a085e495f825ec6e213edfdb7
Grzegorz Pabian <greg.pabian@gmail.com>
2014-12-04 14:53:25 +0100

    Improved operation list formatting.

--- js/main.js
+++ js/main.js
@@ -8,10 +8,19 @@ var editor = window.editor = new Editor( '#editor' );
 
 var html = [];
 
+function formatAttributes( attributes ) {
+	return Object.keys( attributes )
+		.sort()
+		.map( function( attr ) {
+			return attr + ': ' + attributes[ attr ];
+		} )
+		.join( '<br>' );
+}
+
 html = editor.document.data.map( function( op ) {
 	return '<tr><td>' + op.insert + '</td>' +
-		'<td class="' + ( op.attributes ? op.attributes.type ? 'tag' : 'style' : '' ) + '">' +
-		( op.attributes ? JSON.stringify( op.attributes ) : '' ) +
+		'<td class="' + ( op.attributes ? op.attributes.type ? 'tag' : 'style' : 'none' ) + '">' +
+		( op.attributes ? formatAttributes( op.attributes ) : '' ) +
 		'</td></tr>';
 } );
 
