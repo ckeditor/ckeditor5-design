d7f0a19a3e21d8b1c3feb54e483c5054054610fd
Grzegorz Pabian <greg.pabian@gmail.com>
2015-01-28 14:45:00 +0100

    Updated Store class API.

--- src/store.js
+++ src/store.js
@@ -6,8 +6,38 @@ define( [ 'tools/utils' ], function( utils ) {
 		this.hashes = {};
 	}
 
-	utils.extend( Store.prototype, {
+	// produce a "hash" of an item
+	function hash( value ) {
+		// TODO does the order of attributes matter? if not, then should we sort them first?
+		return JSON.stringify( value );
+	}
 
+	utils.extend( Store.prototype, {
+		// store a value in the store and return its index
+		store: function( value ) {
+			var hash = hash( value ),
+				idx = this.hashes[ hash ];
+
+			// this is a new hash
+			if ( idx === undefined ) {
+				idx = this.values.push( value ) - 1;
+				this.hashes[ hash ] = idx;
+			}
+
+			return idx;
+		},
+
+		// return an item on a given position
+		get: function( idx ) {
+			return this.values[ idx ] || null;
+		},
+
+		// return an index of a given value (if any)
+		getIndex: function( value ) {
+			var hash = hash( value );
+
+			return hash in this.hashes ? this.hashes[ hash ] : null;
+		}
 
 	} );
 
