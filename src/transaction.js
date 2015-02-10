8483a385a95069038589fd799bd5742d7637be04
Grzegorz Pabian <greg.pabian@gmail.com>
2015-02-10 15:39:06 +0100

    Optimized node length update process.

--- src/transaction.js
+++ src/transaction.js
@@ -120,29 +120,47 @@ define( [
 	utils.extend( Transaction.prototype, {
 		// apply a transaction to a document
 		applyTo: function( document ) {
-			var offset = 0,
-				data = document.data;
+			var toUpdate = [];
+			var deltas = [];
 
-			// TODO we need to update the document tree:
-			// - insert new nodes
-			// - update lengths
-			// - ???
-
-			// TODO optimize this process
-			function updateLengths( offset, delta ) {
+			// save a node on the given offset and the delta for it to update lengths in the document tree just once
+			function saveToUpdateLength( offset, delta ) {
 				var node = document.getNodeAtPosition( offset );
 
-				// we haven't found a text node
 				// TODO this is just a silly workaround
-				node = document.getNodeAtPosition( offset - 1 );
-
-				while ( node ) {
-					node.adjustLength( delta );
-					node = node.parent;
+				// we haven't found a text node, this usually happens when the carret is at the end of a text node
+				if ( !node.isWrapped ) {
+					node = document.getNodeAtPosition( offset - 1 );
 				}
+
+				var idx = toUpdate.indexOf( node );
+
+				if ( idx === -1 ) {
+					idx = toUpdate.push( node ) - 1;
+				}
+
+				var oldDelta = deltas[ idx ] || 0;
+
+				deltas[ idx ] = oldDelta + delta;
 			}
 
+			// update lengths of nodes and their parents
+			function updateLengths() {
+				toUpdate.forEach( function( node, i ) {
+					var delta = deltas[ i ];
+
+					while ( node ) {
+						node.adjustLength( delta );
+						node = node.parent;
+					}
+				} );
+			}
+
+			var offset = 0;
+
 			this.operations.forEach( function( operation ) {
+				var node, type;
+
 				// update the offset
 				if ( operation.retain ) {
 					offset += operation.retain;
@@ -150,26 +168,33 @@ define( [
 
 				// insert new data
 				if ( operation.insert ) {
-					data.splice( offset, 0, operation.insert );
+					// update the linear data
+					document.data.splice( offset, 0, operation.insert );
 
+					// it's a text content so just update the lengths of nodes and their parents
 					if ( utils.isString( operation.insert ) || utils.isArray( operation.insert ) ) {
-						updateLengths( offset, 1 );
-					} else {
-						// TODO we added a new node so we need to rebuild the tree somehow
+						saveToUpdateLength( offset, 1 );
+					} else if ( utils.isObject( operation.insert ) ) {
+						// TODO
 					}
+
 					offset++;
 				}
 
 				if ( operation.remove ) {
-					data.splice( offset, 1 );
+					// update the linear data
+					document.data.splice( offset, 1 );
+
+					// it's a text content so just update the lengths of nodes and their parents
 					if ( utils.isString( operation.remove ) || utils.isArray( operation.remove ) ) {
-						updateLengths( offset, -1 );
+						saveToUpdateLength( offset, -1 );
 					} else {
-						// TODO we removed a node's opening/closing element
-						// we need to rebuild the tree somehow
+						// TODO
 					}
 				}
 			}, this );
+
+			updateLengths();
 
 			this.applied = true;
 		},
