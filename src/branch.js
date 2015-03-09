cb87047cc378b290b5dd5b4e2172429487fbd2d3
Grzegorz Pabian <greg.pabian@gmail.com>
2015-03-09 14:01:36 +0100

    Revert "Debounce the mutation callback to support IME."
    This reverts commit c1591700a69c80687877bc69c45cf28711622d89.

--- src/branch.js
+++ src/branch.js
@@ -77,7 +77,7 @@ define( [
 			if ( removed.length ) {
 				// remove all elements between the left and right anchors
 				if ( leftAnchor && rightAnchor ) {
-					// console.log( 'remove elements between left and right anchors' );
+					console.log( 'remove elements between left and right anchors' );
 					var rightElem = rightAnchor.view.getElement();
 
 					while (
@@ -88,19 +88,19 @@ define( [
 					}
 					// remove all elements after the left anchor
 				} else if ( leftAnchor ) {
-					// console.log( 'remove elements after the left anchor' );
+					console.log( 'remove elements after the left anchor' );
 					while ( ( sibling = leftAnchor.view.nextSibling ) ) {
 						sibling.parentElement.removeChild( sibling );
 					}
 					// remove all elements before the right anchor
 				} else if ( rightAnchor ) {
-					// console.log( 'remove elements before the right anchor' );
+					console.log( 'remove elements before the right anchor' );
 					while ( ( sibling = rightAnchor.view.previousSibling ) ) {
 						sibling.parentElement.removeChild( sibling );
 					}
 					// remove all children
 				} else {
-					// console.log( 'remove all child elements' );
+					console.log( 'remove all child elements' );
 					this.view.html( '' );
 				}
 			}
