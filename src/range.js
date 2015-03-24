a6fc36d08ad54886405966abb7bb5a5e9edfa054
Grzegorz Pabian <greg.pabian@gmail.com>
2015-03-24 15:51:06 +0100

    Refactored Range class to ue Position objects.

--- src/range.js
+++ src/range.js
@@ -1,63 +1,55 @@
 define( [
+	'position',
 	'tools/utils'
 ], function(
+	Position,
 	utils
 ) {
 	'use strict';
 
-	function Range( from, to ) {
-		this.from = from.offset;
-		this.fromAttributes = from.attributes;
-
-		this.to = to ? to.offset : this.from;
-		this.toAttributes = to ? to.attributes : this.fromAttributes;
-
-		this.start = this.from > this.to ? this.to : this.from;
-		this.startAttributes = this.from > this.to ? this.toAttributes : this.fromAttributes;
-
-		this.end = this.from > this.to ? this.from : this.to;
-		this.endAttributes = this.from > this.to ? this.fromAttributes : this.toAttributes;
+	function Range( start, end ) {
+		this.start = start;
+		this.end = end || start.clone();
 	}
+
+	// static
+	utils.extend( Range, {
+		createFromNativeRange: function( range, document ) {
+			var start, end;
+
+			start = document.getOffsetAndAttributes(
+				range.startContainer,
+				range.startOffset
+			);
+
+			if ( !range.collapsed ) {
+				end = document.getOffsetAndAttributes(
+					range.endContainer,
+					range.endOffset
+				);
+			}
+
+			return new Range( start, end );
+		}
+	} );
 
 	// prototype
 	Object.defineProperty( Range.prototype, 'collapsed', {
 		get: function() {
-			return this.start === this.end;
-		}
-	} );
-
-	Object.defineProperty( Range.prototype, 'length', {
-		get: function() {
-			return this.end - this.start;
+			return this.start.equals( this.end );
 		}
 	} );
 
 	utils.extend( Range.prototype, {
 		equals: function( range ) {
-			return range && range.from === this.from && range.to === this.to &&
-				areEqualArrays( range.fromAttributes, this.fromAttributes ) &&
-				areEqualArrays( range.toAttributes, this.toAttributes );
-		},
-
-		equalsSelection: function( range ) {
-			return range && range.start === this.start && range.end === this.end &&
-				areEqualArrays( range.startAttributes, this.startAttributes ) &&
-				areEqualArrays( range.endAttributes, this.endAttributes );
+			return range && this.start.equals( range.start ) && this.end.equals( range.end );
 		},
 
 		translateOffset: function( offset ) {
-			return new this.constructor( this.from + offset, this.to + offset );
+			this.start.translate( offset );
+			this.end.translate( offset );
 		}
-
-		// TODO transalte attributes
 	} );
-
-	function areEqualArrays( a, b ) {
-		return Array.isArray( a ) && Array.isArray( b ) && a.length === b.length &&
-			a.every( function( item, i ) {
-				return item === b[ i ];
-			} );
-	}
 
 	return Range;
 
