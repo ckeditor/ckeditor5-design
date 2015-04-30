8d71ab1c64e2b3d7464d568b42fdab9328f6cc5c
Grzegorz Pabian <greg.pabian@gmail.com>
2015-04-30 14:33:15 +0200

    Fixed NodeManager.register() and .unregister().

--- src/nodemanager.js
+++ src/nodemanager.js
@@ -7,10 +7,23 @@ define( function() {
 	}
 
 	NodeManager.prototype = {
+		// clear all registered nodes
+		clear: function() {
+			this._list = [];
+			this._items = {};
+		},
+
 		// create a node from the given data if a proper node type constructor exists
 		create: function( data ) {
 			var constructor = this._items[ typeof data === 'string' ? data : data.type ];
 			return constructor ? new constructor[ 1 ]( data ) : null;
+		},
+
+		// return a node constructor with the given name or all the constructors
+		get: function( name ) {
+			return name !== undefined ? this._items[ name ][ 1 ] : this._list.map( function( item ) {
+				return item[ 1 ];
+			} );
 		},
 
 		// return an ordered list of node constructors names
@@ -18,6 +31,22 @@ define( function() {
 			return this._list.map( function( item ) {
 				return item[ 0 ];
 			} );
+		},
+
+		// find a style constructor for the given data
+		matchStyleForData: function( data ) {
+			var result = null;
+
+			Object.keys( data ).some( function( name ) {
+				// we use booleans for style flags only (for now at least)
+				if ( data[ name ] === true && this._items[ name ] ) {
+					result = this._items[ name ][ 1 ];
+
+					return true;
+				}
+			}, this );
+
+			return result;
 		},
 
 		// register a constructor for a node type, default priority = 10
@@ -37,7 +66,7 @@ define( function() {
 
 			this._items[ type ] = item;
 
-			for ( var i = this._list.length - 1; i > 0; i-- ) {
+			for ( var i = this._list.length - 1; i >= 0; i-- ) {
 				if ( this._list[ i ][ 2 ] <= priority ) {
 					this._list.splice( i + 1, 0, item );
 					return;
@@ -51,35 +80,12 @@ define( function() {
 		unregister: function( name ) {
 			delete this._items[ name ];
 
-			for ( var i = this.length - 1; i >= 0; i-- ) {
+			for ( var i = this._list.length - 1; i >= 0; i-- ) {
 				if ( this._list[ i ][ 0 ] === name ) {
 					this._list.splice( i, 1 );
 					return;
 				}
 			}
-		},
-
-		// find a style constructor for the given data
-		matchStyleForData: function( data ) {
-			var result = null;
-
-			Object.keys( data ).some( function( name ) {
-				// we use booleans for style flags only (for now at least)
-				if ( data[ name ] === true && this._items[ name ] ) {
-					result = this._items[ name ][ 1 ];
-
-					return true;
-				}
-			}, this );
-
-			return result;
-		},
-
-		// return a node constructor with the given name or all the constructors
-		get: function( name ) {
-			return name !== undefined ? this._items[ name ][ 1 ] : this._list.map( function( item ) {
-				return item[ 1 ];
-			} );
 		}
 	};
 
