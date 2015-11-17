9a0275d9f12c5a748e12516c3a8b50df5b53929d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-11-17 22:38:02 +0100

    Foo

---
+++ ckeditor.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import Editor from 'ckeditor5-core/editor.js';
+import proof from 'src/proof.js';
+
+proof();
+
+const CKEDITOR = {
+	create( config ) {
+		console.log( 'CKEDITOR.create()' );
+
+		const editor = new Editor( config.features );
+
+		return editor.init();
+	}
+};
+
+export default CKEDITOR;
---
+++ src/proof.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default function proof() {
+	console.log( 'proof()' );
+}
