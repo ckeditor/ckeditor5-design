42a599c1228a08cea215ff4e1f825b958bdfdcbb
Maciej Bukowski <maciejbukowski@Admins-MacBook-Pro.local>
2016-10-10 18:36:43 +0200

    Added task to esdoc json output

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const ESDoc = require('esdoc');
+const gulp = require('gulp');
+const fs  = require('fs');
+const path = require('path');
+const config = require('./esdoc.json');
+
+const destination = path.resolve(__dirname, config.destination);
+
+/** Create json file using esdoc.json config */
+gulp.task('esdoc:json', () => {
+	ESDoc.generate(config, (results, files) => {
+		const json = JSON.stringify(results);
+		fs.writeFile(destination, json, err => {
+			if (err)
+				throw err;
+		});
+	});
+});
