f831a42e7ffe8cbf2975adf7874e22b19e149e55
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-10-11 16:55:14 +0200

    Removed noise.

--- gulpfile.js
+++ gulpfile.js
@@ -2,59 +2,30 @@
 
 'use strict';
 
-const ESDoc = require( 'esdoc' );
 const gulp = require( 'gulp' );
-const fs = require( 'fs' );
 const path = require( 'path' );
 const del = require( 'del' );
 const sh = require( 'shelljs' );
-const jsonStringify = require( 'json-pretty' );
 
 gulp.task( 'default', [ 'build:all' ] );
 
-gulp.task( 'build:all', [ 'build:esdoc:json', 'build:esdoc:html', 'build:jsdoc:html', 'build:documentationjs:json' ] );
+gulp.task( 'build:all', [ 'build:documentationjs:json', 'build:documentationjs:html' ] );
 
-/** Create json file using esdoc.json config */
-gulp.task( 'build:esdoc:json', [ 'clean:esdoc' ], ( done ) => {
-	const esdocConfig = require( './esdoc/config.json' );
-	const destination = path.join( __dirname, 'esdoc', 'out-json' );
-	esdocConfig.destination = destination;
-
-	ESDoc.generate( esdocConfig, ( results, asts, config ) => {
-		const json = jsonStringify( results );
-
-		fs.mkdirSync( destination );
-		fs.writeFileSync( path.join( destination, 'output.json' ), json );
-
-		done();
-	} );
-} );
-
-gulp.task( 'build:esdoc:html', [ 'clean:esdoc' ], () => {
-	sh.exec( `./node_modules/.bin/esdoc -c ./esdoc/config.json` );
-} );
-
-gulp.task( 'clean:esdoc', () => {
-	const destination1 = path.resolve( __dirname, 'esdoc', 'out-json' );
-	const destination2 = path.resolve( __dirname, 'esdoc', 'out-html' );
+gulp.task( 'clean:documentationjs', () => {
+	const destination1 = path.resolve( __dirname, 'documentationjs', 'out-json' );
+	const destination2 = path.resolve( __dirname, 'documentationjs', 'out-html' );
 
 	return del( destination1, destination2 );
 } );
 
-gulp.task( 'build:jsdoc:html', () => {
-	return del( './jsdoc/out' )
-		.then( () => {
-			sh.exec( './node_modules/.bin/jsdoc -c jsdoc/config.json --debug ckeditor5 -d jsdoc/out' );
-		} );
+gulp.task( 'build:documentationjs:json' , [ 'clean:documentationjs' ], () => {
+	const destination = path.resolve( __dirname, 'documentationjs', 'out-json' );
+
+	sh.exec( `./node_modules/.bin/documentation build ckeditor5 -f json -o ${ destination }` );
 } );
 
-gulp.task( 'clean:decumentationjs', () => {
-	const { destination } = require( './documentationjs/config.json' );
+gulp.task( 'build:documentationjs:html' , [ 'clean:documentationjs' ], () => {
+	const destination = path.resolve( __dirname, 'documentationjs', 'out-html' );
 
-	return del( destination );
-} );
-
-gulp.task( 'build:documentationjs:json' , [ 'clean:decumentationjs' ], () => {
-	const { format, destination } = require( './documentationjs/config.json' );
-	sh.exec( `./node_modules/.bin/documentation build ckeditor5 -f ${ format } -o ${ destination }` );
+	sh.exec( `./node_modules/.bin/documentation build ckeditor5 -f html -o ${ destination }` );
 } );
