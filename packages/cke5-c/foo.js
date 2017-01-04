f009c5d013ee07a47d5985f37fc874398af2af08
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-01-04 14:28:01 +0100

    Now it's also possible to test it in Node.

--- packages/cke5-c/foo.js
+++ packages/cke5-c/foo.js
@@ -1,3 +1,5 @@
+const r = Math.random();
+
 module.exports = function c2() {
-	console.log( 'c2' );
+	console.log( 'c2', r );
 };
--- packages/cke5-c/index.js
+++ packages/cke5-c/index.js
@@ -1,3 +1,5 @@
+const r = Math.random();
+
 module.exports = function c1() {
-	console.log( 'c1' );
+	console.log( 'c1', r );
 };
